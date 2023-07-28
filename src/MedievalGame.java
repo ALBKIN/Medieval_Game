6339aa10c8d4ccc081583d12a4b7f5cf510931f7
ALBKIN <kinkel.albert@gmail.com>
2023-07-28 22:38:18 +0200

    First run with "Test Player" Still lacking control methods implementation

--- src/MedievalGame.java
+++ src/MedievalGame.java
@@ -1,5 +1,81 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+import java.util.Objects;
+
 public class MedievalGame {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    /* Instance Variables */
+    private Player player;
+
+    /* Main Method */
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
+        Scanner console = new Scanner(System.in);
+        MedievalGame game = new MedievalGame();
+
+        game.player = game.start(console);
+
+        game.addDelay(500);
+        System.out.println("\nLet's take a quick look at you to make sure you're ready to head out the door.");
+        System.out.println(game.player);
+
+        game.addDelay(1000);
+        System.out.println("\nWell, you're off to a good start, let's get your game saved so we don't lose it.");
+        game.save();
+
+        game.addDelay(2000);
+        System.out.println("We just saved your game...");
+        System.out.println("Now we are going to try to load your character to make sure the save worked...");
+
+        game.addDelay(1000);
+        System.out.println("Deleting character...");
+        String charName = game.player.getName();
+        game.player = null;
+
+        game.addDelay(1500);
+        game.player = game.load(charName, console);
+        System.out.println("Loading character...");
+
+        game.addDelay(2000);
+        System.out.println("Now let's print out your character again to make sure everything loaded:");
+
+        game.addDelay(500);
+        System.out.println(game.player);
+    } // End of main
+
+    /* Instance Methods */
+    private Player start(Scanner console) {
+        // TODO: Add start functionality here
+
+        return new Player("Test");
+    } // End of start
+
+    private void save() throws FileNotFoundException, IOException {
+        // TODO: Add save functionality here
+        String fileName = player.getName() + ".svr";
+        FileOutputStream userSaveFile = new FileOutputStream(fileName);
+        ObjectOutputStream playerSaver = new ObjectOutputStream(userSaveFile);
+
+    } // End of save
+
+    private Player load(String playerName, Scanner console) {
+        // TODO: Add load functionality here
+
+        return new Player("Test");
+    } // End of load
+
+    /*
+    Adds a delay to the console so it seems like the computer is "thinking"
+    or "responding" like a human, not instantly like a computer.
+    */
+    private void addDelay(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
