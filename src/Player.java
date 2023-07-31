25702173f6dc28a4b66350295aaab5506a870007
ALBKIN <kinkel.albert@gmail.com>
2023-07-31 10:09:22 +0200

    Added .getAttackPoints() for Combat

--- src/Player.java
+++ src/Player.java
@@ -58,6 +58,10 @@ public class Player implements Serializable {
         this.currentWeapon = currentWeapon;
     }
 
+    public double getAttackPoints() {
+        return currentWeapon.getDamage();
+    }
+
     public double getHealth() {
         return health;
     }
